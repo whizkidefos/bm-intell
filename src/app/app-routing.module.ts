0fa256eff3c96d22ba959b66b035e2ecb8420ccb
Efosa Igbinehi <liova2273@gmail.com>
2019-09-05 18:19:21 +0200

    setup routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SectionSalesComponent } from './sections/section-sales/section-sales.component';
+import { SectionOrdersComponent } from './sections/section-orders/section-orders.component';
+import { SectionHealthComponent } from './sections/section-health/section-health.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'sales', component: SectionSalesComponent },
+  { path: 'orders', component: SectionOrdersComponent },
+  { path: 'health', component: SectionHealthComponent },
+
+  { path: '', redirectTo: '/sales', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { SectionSalesComponent } from './sections/section-sales/section-sales.component';
+import { SectionHealthComponent } from './sections/section-health/section-health.component';
+import { SectionOrdersComponent } from './sections/section-orders/section-orders.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    SidebarComponent
+    SidebarComponent,
+    SectionSalesComponent,
+    SectionHealthComponent,
+    SectionOrdersComponent
   ],
   imports: [
     BrowserModule,
